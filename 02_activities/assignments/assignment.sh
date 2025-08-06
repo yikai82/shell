d8bc95df649f068214a519e72c14c28acb1f0f2d
yikai82 <yikai.su89@gmail.com>
2025-08-06 15:29:58 -0400

    trace -x OFF, remove ./

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x # debug trace ON
+# set -x # debug trace OFF
 
 # === COLORS ===
 GREEN='\033[1;32m'; YELLOW='\033[1;33m'; MAGENTA='\033[0;35m'; NC='\033[0m'
@@ -33,22 +33,22 @@ echo -e "${GREEN}Status: file unzipped${NC}"
 # Complete assignment here
 
 # 1. Create a directory named data
-mkdir ./data
+mkdir data
 echo -e "${GREEN}Status: created folder: ./data${NC}"
 
 # 2. Move the ./rawdata directory to ./data/raw
-mkdir -p ./data/raw
-mv ./rawdata/* ./data/raw
+mkdir data/raw
+mv rawdata/* data/raw
 echo -e "${GREEN}Status: files moved to ./data/raw${NC}"
 
 # 3. List the contents of the ./data/raw directory
 echo -e "${GREEN}Status: listing the content in the working folder:./data/raw${NC}"
-ls ./data/raw/
+ls data/raw/
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-mkdir -p ./data/processed/server_logs
-mkdir -p ./data/processed/user_logs
-mkdir -p ./data/processed/event_logs
+mkdir -p data/processed/server_logs
+mkdir -p data/processed/user_logs
+mkdir -p data/processed/event_logs
 
 echo -e "${GREEN}Status: create the following directories:
             ./data/processed/server_logs
@@ -57,25 +57,22 @@ echo -e "${GREEN}Status: create the following directories:
             ${NC}"
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp ./data/raw/server_*.log  ./data/processed/server_logs
+cp data/raw/server_*.log  data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
-cp ./data/raw/user_*.log  ./data/processed/user_logs
-cp ./data/raw/event_*.log  ./data/processed/event_logs
+cp data/raw/user_*.log  data/processed/user_logs
+cp data/raw/event_*.log  data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm ./data/raw/*ipaddr*
-rm ./data/processed/user_logs/*ipaddr*
+rm data/raw/*ipaddr*
+rm data/processed/user_logs/*ipaddr*
 echo -e "${YELLOW}Status: deleting privacy related files${NC}"
-ls -R ./data/processed | grep ipaddr 
+ls -R data/processed | grep ipaddr 
 
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 echo -e "${GREEN}Status: creating inventory.txt${NC}"
-# ls -R ./data/processed > ./data/inventory.txt
-# ls -R ./data/processed # output to terminal for visual check
-
-find ./data/processed -type f > ./data/inventory.txt
+find data/processed -type f > data/inventory.txt
 
 ###########################################
 echo -e "${GREEN}Status: Project setup is complete!${NC}"
