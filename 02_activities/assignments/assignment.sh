6a4c790f6f6b2f7696999a1d56ec9aa46d6874a6
yikai82 <yikai.su89@gmail.com>
2025-08-06 23:40:20 -0400

    set -x ON; colored output

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -x # debug trace ON
 
-# === COLORS ===
-GREEN='\033[1;32m'; YELLOW='\033[1;33m'; MAGENTA='\033[0;35m'; NC='\033[0m'
-echo -e "${MAGENTA}############################################${NC}"
-echo -e "${MAGENTA}# DSI CONSULTING INC. Project setup script #${NC}"
-echo -e "${MAGENTA}############################################${NC}"
-echo -e "${MAGENTA}# This script creates standard analysis and output directories${NC}"
-echo -e "${MAGENTA}# for a new project. It also creates a README file with the${NC}"
-echo -e "${MAGENTA}# project name and a brief description of the project.${NC}"
-echo -e "${MAGENTA}# Then it unzips the raw data provided by the client.${NC}"
+echo -e "\033[0;35m############################################\033[0m"
+echo -e "\033[0;35m# DSI CONSULTING INC. Project setup script #\033[0m"
+echo -e "\033[0;35m############################################\033[0m"
+echo -e "\033[0;35m# This script creates standard analysis and output directories\033[0m"
+echo -e "\033[0;35m# for a new project. It also creates a README file with the\033[0m"
+echo -e "\033[0;35m# project name and a brief description of the project.\033[0m"
+echo -e "\033[0;35m# Then it unzips the raw data provided by the client.\033[0m"
 
 if [ -d newproject ]; then
   echo "Directory 'newproject' already exists. Please remove it before running this script."
@@ -26,22 +24,22 @@ touch analysis/main.py
 # download client data
 curl -Lo rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
 unzip -q rawdata.zip
-echo -e "${GREEN}Status: file unzipped${NC}"
+echo -e "\033[1;32mStatus: file unzipped\033[0m"
 
 ###########################################
 # Complete assignment here
 
 # 1. Create a directory named data
 mkdir data
-echo -e "${GREEN}Status: created folder: ./edata${NC}"
+echo -e "\033[1;32mStatus: created folder: ./edata\033[0m"
 
 # 2. Move the ./rawdata directory to ./data/raw
 mkdir data/raw
 mv rawdata data/raw
-echo -e "${GREEN}Status: files moved to ./data/raw${NC}"
+echo -e "\033[1;32mStatus: files moved to ./data/raw\033[0m"
 
 # 3. List the contents of the ./data/raw directory
-echo -e "${GREEN}Status: listing the content in the working folder:./data/raw${NC}"
+echo -e "\033[1;32mStatus: listing the content in the working folder:./data/raw\033[0m"
 ls data/raw/
 
 # 4. In ./data/processed, create the following directories:cp data/raw/server_*.log  data/processed/server_logs server_logs, user_logs, and event_logs
@@ -49,11 +47,11 @@ mkdir -p data/processed/server_logs
 mkdir -p data/processed/user_logs
 mkdir -p data/processed/event_logs
 
-echo -e "${GREEN}Status: create the following directories:
+echo -e "\033[1;32mStatus: create the following directories:
             ./data/processed/server_logs
             ./data/processed/user_logs
             ./data/processed/event_logs
-            ${NC}"
+            \033[0m"
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cp data/raw/rawdata/server_*.log  data/processed/server_logs
@@ -63,14 +61,14 @@ cp data/raw/rawdata/user_*.log  data/processed/user_logs
 cp data/raw/rawdata/event_*.log  data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm data/raw/*ipaddr*
+rm data/raw/rawdata/*ipaddr*
 rm data/processed/user_logs/*ipaddr*
-echo -e "${YELLOW}Status: deleting privacy related files${NC}"
+echo -e "\033[1;33mStatus: deleting privacy related files\033[0m"
 ls -R data/processed | grep ipaddr 
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-echo -e "${GREEN}Status: creating inventory.txt${NC}"
+echo -e "\033[1;32mStatus: creating inventory.txt\033[0m"
 find data/processed -type f > data/inventory.txt
 
 ###########################################
-echo -e "${GREEN}Status: Project setup is complete!${NC}"
+echo -e "\033[1;32mStatus: Project setup is complete!\033[0m"
